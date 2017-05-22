07eacb861c6f7aed8eae31eca102c0f320466fd2
eliott enos <eliottenos@gmail.com>
2017-05-22 15:00:28 -0400

    Upload modal
    now able to open and close upload modal and display file name @pjliddy

--- assets/scripts/view.js
+++ assets/scripts/view.js
@@ -198,19 +198,20 @@ const showChangePasswordFailure = () => {
   // showAlert(`error`, `For highly complex reasons, your password couldn't be changed.`)
 }
 
-const showUpload = (id) => {
+const showUpload = () => {
   // render handlebars template with data-id for item
   // const contentTemplate = require('./templates/modal-confirm-delete.handlebars')
   // const content = contentTemplate({id: id})
+  appendView('body', 'modal-upload')
 
   // if there's already a modal
-  if ($('#upload-modal').length) {
-    // replace the existing modal
-    renderView('#upload-modal', 'modal-upload')
-  } else {
-    // insert a new alert
-    appendView('body', 'modal-upload')
-  }
+  // if ($('#upload-modal').length) {
+  //   // replace the existing modal
+  //   renderView('#upload-modal', 'modal-upload')
+  // } else {
+  //   // insert a new alert
+  //   appendView('body', 'modal-upload')
+  // }
 
   // show the hidden modal
   $('#upload-modal').modal('show')
